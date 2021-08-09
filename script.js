4047fc03ee4d5a20a6685ac6d45a743a230ec8b7
Anthios314 <55056986+Anthios314@users.noreply.github.com>
2021-08-09 17:13:23 -0400

    Added functionality
    - Mines now display when the game ends
    - Trying to add some other stuff re: tiles and efficiency, but I'm working on other stuff first.

--- script.js
+++ script.js
@@ -4,6 +4,72 @@ let gridRows = 10;
 let gridCols = 10;
 let unflaggedMines = 10;
 
+class Tile {
+    constructor (data) { // Data contains x, y, and isMine. 
+        this.x = data.x;
+        this.y = data.y;
+        this.isMine = data.isMine;
+        this.isFlagged = false;
+        this.uncovered = false;
+    }
+}
+
+class MineArray {
+    constructor (data) { // Data contains rows, cols, and minesTotal
+        this.rows = data.rows;
+        this.cols = data.cols;
+        this.minesTotal = data.minesTotal;
+
+        this.minesLeft = data.minesTotal; // The number of mines that have yet to be uncovered.
+        this.tilesLeft = this.rows * this.cols;
+    }
+    
+    get isMine(x,y) { // Tell me if a tile contains a mine. 
+        return (x >= 0 && y >= 0 && x < this.rows && y < this.cols && this[x][y].isMine);
+    }
+
+    get minesNear(x, y) {
+        let nearByMines = 0;
+        if (this.isMine(x+1, y+1)) nearbyMines++;
+        if (this.isMine(x+1, y  )) nearbyMines++;
+        if (this.isMine(x+1, y-1)) nearbyMines++;
+
+        if (this.isMine(x-1, y+1)) nearbyMines++;
+        if (this.isMine(x-1, y  )) nearbyMines++;
+        if (this.isMine(x-1, y-1)) nearbyMines++;
+
+        if (this.isMine(x  , y+1)) nearbyMines++;
+        if (this.isMine(x  , y-1)) nearbyMines++;
+
+        return nearByMines;
+    }
+
+    get displayHTML() { // Creates the tile that is rendered on the screen. 
+        let classes = "class=", click = "onclick=";
+        if (!gameHasEnded) {
+            if (this.isMine) {
+                classes += `"tile mine covered"`
+                click += `"endGame()"` //This thing is shit. FIX IT. 
+            } else {
+                classes += `"tile covered"`
+                click += `"uncoverTile(${this.x},${this.y})"`
+            }
+        } else {
+
+        }
+        return `<div 
+            id="tile-${this.x}-${this.y}" 
+            class="tile covered" 
+            onclick="uncoverTile(${this.x},${this.y})"
+            oncontextmenu="flagTile(${this.x},${this.y})">
+            ${this.nearbyMines}</div>`;
+    }
+}
+
+
+
+
+
 function generateGridFromInput() {// Takes the input data and creates a milefield. 
     try {
     generateGrid(
@@ -69,6 +135,8 @@ function generateGrid(cols, rows, mines) { // Function that generates a 2D mines
 
 function displayGrid(matrix) {
     let string = "";
+    let vars = document.querySelector(':root')
+    vars.style.setProperty("--tilesWidth", 30 * matrix.length)
     for (let i in matrix) {
         string += `<div id="row-${i}" class="mine-row">`
         for (let j in matrix[i]) {
@@ -88,10 +156,11 @@ function displayGrid(matrix) {
 }
 
 function uncoverTile(x, y, auto = false) { // Uncovers a tile.
+    if (gameHasEnded) return false;
     let tile = document.getElementById(`tile-${x}-${y}`).classList;
     let tileData = getTileData(x,y)
 
-    if (tile.contains("uncovered") || tile.contains("flagged")) return;
+    if (tile.contains("uncovered") || tile.contains("flagged")) return false;
     tile.add("uncovered");
     tile.remove("covered");
 
@@ -111,11 +180,13 @@ function uncoverTile(x, y, auto = false) { // Uncovers a tile.
         if (mineGrid[x][y - 1] !== undefined) uncoverTile(x, y-1, true);
 
     }
+    if (tileData === "X") onMineClicked();
 }
 
 function flagTile(x,y) {
+    if (gameHasEnded) return false;
     let tile = document.getElementById(`tile-${x}-${y}`).classList;
-    if (tile.contains("uncovered")) return;
+    if (tile.contains("uncovered")) return false;
 
     if (tile.contains("flagged")) tile.remove("flagged");
     else tile.add("flagged");
@@ -132,4 +203,15 @@ function RandBetween(min, max, isInt = true) { // A function that creates a rand
     let number =  Math.random() * (max-min) + min;
     if (isInt) return Math.floor(number); // isInt = should the number that is returned be an integer? 
     return number;
+}
+
+function onMineClicked() {
+    gameHasEnded = true;
+    let element;
+    for (i in mineGrid) {
+        for (j in mineGrid[i]) {
+            element = document.getElementById(`tile-${i}-${j}`).classList
+            if (mineGrid[i][j] === "X" && !element.contains("flagged")) element.add("mine");
+        }
+    }
 }
